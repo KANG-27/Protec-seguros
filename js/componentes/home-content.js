527c32b5e728a3c3e27b39f149df1d20c1d4b783
Kevin Niño <kevinandresrg@gmail.com>
2025-07-25 12:42:04 -0500

    create carrousel para aliados

--- js/componentes/home-content.js
+++ js/componentes/home-content.js
@@ -27,19 +27,67 @@ class HomeContent extends HTMLElement {
         <div class="home__aliados">
           <h3>Nuestros aliados</h3>
           <p>Conectamos con las mejores aseguradoras para darte siempre lo mejor</p>
-          <div class="home__aliados__logos">
-            <img src="./img/sura.png" alt="Aliados" class="logo-aliados">
-            <img src="./img/aseguradoraSolidaria.png" alt="Aliados" class="logo-aliados">
-            <img src="./img/mapfre.png" alt="Aliados" class="logo-aliados">
-            <img src="./img/axaColpatria.png" alt="Aliados" class="logo-aliados">
-            <img src="./img/segurosBolivar.png" alt="Aliados" class="logo-aliados">
-            <img src="./img/segurosDelEstado.png" alt="Aliados" class="logo-aliados">
+          <div class="home__aliados__logos__carrousel" id="aliados-carousel">
+            <div class="home__aliados__logos__carrousel__track">
+              <div class="carousel__slide">
+                <img src="./img/sura.png" alt="Aliados" >
+              </div>
+              <div class="carousel__slide">
+                <img src="./img/aseguradoraSolidaria.png" alt="Aliados" >
+              </div>
+              <div class="carousel__slide">
+              <img src="./img/mapfre.png" alt="Aliados" >
+              </div>
+              <div class="carousel__slide">
+                <img src="./img/axaColpatria.png" alt="Aliados" >
+              </div>
+              <div class="carousel__slide">
+                <img src="./img/segurosBolivar.png" alt="Aliados" >
+              </div>
+              <div class="carousel__slide">
+                <img src="./img/segurosDelEstado.png" alt="Aliados" >
+              </div>
+            </div>
           </div>
         </div>
         <h3 class="titulo-servicios">Contactanos</h3>
         <contact-component></contact-component>
       </section>
     `;
+
+    const track = document.querySelector('#aliados-carousel .home__aliados__logos__carrousel__track')
+    const slides = Array.from(track.children);
+    let index = 0;
+
+    // --- Breakpoints ---
+    function getSlidesPerView() {
+      if (window.matchMedia('(min-width:1024px)').matches) return 3; // desktop
+      if (window.matchMedia('(min-width:768px)').matches) return 2; // tablet
+      return 1;                                                     // mobile
+    }
+
+    let spv = getSlidesPerView(); // slides per view
+
+    // Recalcular al cambiar tamaño
+    window.addEventListener('resize', () => {
+      spv = getSlidesPerView();
+      index = 0;            // opcional: regresar al inicio para evitar saltos raros
+      moverA(index);
+    });
+
+    function moverA(i) {
+      const offset = i * (100);                   // ancho de “página”
+      track.style.transform = `translateX(-${offset}%)`;
+    }
+
+    function siguiente() {
+      const totalPaginas = Math.ceil(slides.length / spv);
+      index = (index + 1) % totalPaginas;
+      moverA(index);
+    }
+
+    setInterval(siguiente, 3000);
+
   }
 }
 
